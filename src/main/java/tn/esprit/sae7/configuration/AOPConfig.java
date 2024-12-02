c3cd1d016dea494d37b7aad0ff130dee3fff516e
TahaNaat <naat.taha@esprit.tn>
2024-12-02 16:23:36 +0100

    corrected calculation error of method

--- src/main/java/tn/esprit/sae7/configuration/AOPConfig.java
+++ src/main/java/tn/esprit/sae7/configuration/AOPConfig.java
@@ -9,24 +9,32 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Component
 @Aspect
 @Slf4j
 public class AOPConfig {
-    @After("execution(public * tn.esprit.sae7.service.*Impl.ajout*(String,..) throws Throwable)")//type de advice
-    @Before("execution(* tn.esprit.sae7.service.*.*(..))")
-    void aspect(JoinPoint joinPoint) {
-        log.info("In method : " + joinPoint.getSignature().getName());//
+
+    @After("execution(public * tn.esprit.sae7.service.*Impl.ajout*(String,..) throws Throwable)") // Type de advice
+    public void afterAdvice(JoinPoint joinPoint) {
+        log.info("After method : " + joinPoint.getSignature().getName());
     }
+
+    @Before("execution(* tn.esprit.sae7.service.*.*(..))")
+    public void beforeAdvice(JoinPoint joinPoint) {
+        log.info("Before method : " + joinPoint.getSignature().getName());
+    }
+
     @Around("execution(* tn.esprit.sae7.service.*.*(..))")
-    Object calculTempsResponse(ProceedingJoinPoint pjp) throws Throwable {
+    public Object calculTempsResponse(ProceedingJoinPoint pjp) throws Throwable {
         LocalDateTime debut = LocalDateTime.now();
         Object obj = pjp.proceed();
         LocalDateTime fin = LocalDateTime.now();
-        long duree = fin.getSecond()-debut.getSecond();
-        log.info("Temps d'execution de la methode : "+ pjp.getSignature().getName()+"est" + duree + " secondes");
+        long duree = Duration.between(debut, fin).toMillis();
+
+        log.info("Temps d'execution de la methode : " + pjp.getSignature().getName() + " est " + duree + " millisecondes");
         return obj;
     }
-}   
+}
